bcf7404f0d794db7630e269db8343027c40a02f3
psmBITW <89567699+psmBITW@users.noreply.github.com>
2021-12-28 18:23:36 +0530

    committed by Partha Sarathi

--- src/screens/WallpaperHome/index.js
+++ src/screens/WallpaperHome/index.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect,Fragment} from 'react';
+import React, { useState, useEffect, Fragment } from "react";
 import {
   SafeAreaView,
   ScrollView,
@@ -13,28 +13,32 @@ import {
   Alert,
   ToastAndroid,
   FlatList,
-  Image
-} from 'react-native';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import {responsiveFontSize,responsiveHeight,responsiveWidth} from 'react-native-responsive-dimensions';
-import axios from 'axios';
-import {useNavigation} from '@react-navigation/native';
-import Toast from 'react-native-simple-toast';
-import Heading from '../../components/Heading';
+  Image,
+} from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import {
+  responsiveFontSize,
+  responsiveHeight,
+  responsiveWidth,
+} from "react-native-responsive-dimensions";
+import axios from "axios";
+import { useNavigation } from "@react-navigation/native";
+import Toast from "react-native-simple-toast";
+import Heading from "../../components/Heading";
 
 const WallpaperHome = () => {
-  const [getData, setData] = useState('');
+  const [getData, setData] = useState("");
   const [isLoading, setLoading] = useState(false);
-  const [pageNum,setPageNum] = useState(1);
-  const [limit,setLimit] = useState(3);
-  const [list,setList] = useState([]);
-  const navigation=useNavigation();
+  const [pageNum, setPageNum] = useState(1);
+  const [limit, setLimit] = useState(3);
+  const [list, setList] = useState([]);
+  const navigation = useNavigation();
 
   useEffect(() => {
-    const unsubscribe = navigation.addListener('focus', () => {
+    const unsubscribe = navigation.addListener("focus", () => {
       // The screen is focused
       // Call any action
-      console.log('inside WallpaperHome');
+      console.log("inside WallpaperHome");
       getWallpapers(1);
       //setPageNum(1);
     });
@@ -43,56 +47,117 @@ const WallpaperHome = () => {
     return unsubscribe;
   }, [getData]);
 
-  async function getWallpapers(pn){
-    await axios.get("https://picsum.photos/v2/list?page="+pn+"&limit="+limit,{timeout:10000})
-    .then(function(response){
-      console.log("wallpaper",response.data);
-      //Toast.showWithGravity("Data fetched successfully!",Toast.SHORT,Toast.BOTTOM);
-      setList(list?[...list,response.data]:response.data);
-    })
-    .catch(function(error){
-      console.log(error);
-      Toast.showWithGravity("Unable to fetch data",Toast.SHORT,Toast.BOTTOM);
-    })
+  async function getWallpapers(pn) {
+    await axios
+      .get("https://picsum.photos/v2/list?page=" + pn + "&limit=" + limit, {
+        timeout: 10000,
+      })
+      .then(function (response) {
+        console.log("wallpaper", response.data);
+        //Toast.showWithGravity("Data fetched successfully!",Toast.SHORT,Toast.BOTTOM);
+        setList(list ? [...list, response.data] : response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+        Toast.showWithGravity(
+          "Unable to fetch data",
+          Toast.SHORT,
+          Toast.BOTTOM
+        );
+      });
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     //alert(JSON.stringify(list));
-    getWallpapers(pageNum)
+    getWallpapers(pageNum);
     //alert(pageNum)
-  },[pageNum])
+  }, [pageNum]);
 
-  return(
+  return (
     <Fragment>
-        <View style={{height:responsiveHeight(6),backgroundColor:'#fff'}}>
-            <StatusBar translucent={true} backgroundColor={"#fff"} barStyle={'dark-content'}/>
-        </View>
-        <SafeAreaView style={{backgroundColor:"#fff",flex:1}}>
-           
-                <Heading pageName={"Wallpapers"} buttonType={'none'}/>
-                {list.length > 0 ?
-                  <FlatList
-                  onScrollEndDrag={()=>{setPageNum(pageNum+1)}}
-                  style={{marginBottom:responsiveWidth(20),paddingBottom:responsiveWidth(10)}}
-                  data={list}
-                  renderItem={({item,index})=>{
-                    return(
-                      <TouchableOpacity style={{height:responsiveHeight(38),width:'98%',backgroundColor:'#808080',borderRadius:responsiveWidth(3),marginBottom:responsiveWidth(2),alignSelf:'center'}}>
-                        <View style={{height:"100%",width:'100%',justifyContent:'center',alignItems:'center'}}><ActivityIndicator size={"large"} color={"#fff"} style={{alignSelf:'center'}} /></View>
-                        <Image source={{uri:item.download_url}} style={{height:"100%",width:'100%',position:'absolute',borderRadius:responsiveWidth(3)}}/>
-                      </TouchableOpacity>
-                    )
+      <View style={{ height: responsiveHeight(6), backgroundColor: "#fff" }}>
+        <StatusBar
+          translucent={true}
+          backgroundColor={"#fff"}
+          barStyle={"dark-content"}
+        />
+      </View>
+      <SafeAreaView style={{ backgroundColor: "#fff", flex: 1 }}>
+        <Heading pageName={"Wallpapers"} buttonType={"none"} />
+        {list.length > 0 ? (
+          <FlatList
+            onScrollEndDrag={() => {
+              setPageNum(pageNum + 1);
+            }}
+            data={list}
+            renderItem={({ item, index }) => {
+              return (
+                <TouchableOpacity
+                  style={{
+                    height: responsiveHeight(38),
+                    width: "98%",
+                    backgroundColor: "#808080",
+                    borderRadius: responsiveWidth(3),
+                    marginBottom: responsiveWidth(2),
+                    alignSelf: "center",
                   }}
+                >
+                  <View
+                    style={{
+                      height: "100%",
+                      width: "100%",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    <ActivityIndicator
+                      size={"large"}
+                      color={"#fff"}
+                      style={{ alignSelf: "center" }}
+                    />
+                  </View>
+                  <Image
+                    source={{ uri: item.download_url }}
+                    style={{
+                      height: "100%",
+                      width: "100%",
+                      position: "absolute",
+                      borderRadius: responsiveWidth(3),
+                    }}
                   />
-                  :
-                  <TouchableOpacity onPress={()=>{getWallpapers(1)}} style={{alignSelf:'center',justifyContent:'center',alignItems:'center',padding:responsiveWidth(5),borderRadius:responsiveWidth(8),backgroundColor:"#000",width:responsiveWidth(50)}}>
-                    <Text style={{color:"#fff",fontSize:responsiveFontSize(2),fontWeight:'bold'}}>Retry</Text>
-                  </TouchableOpacity>
-                }
-  
-            
-        </SafeAreaView>
+                </TouchableOpacity>
+              );
+            }}
+          />
+        ) : (
+          <TouchableOpacity
+            onPress={() => {
+              getWallpapers(1);
+            }}
+            style={{
+              alignSelf: "center",
+              justifyContent: "center",
+              alignItems: "center",
+              padding: responsiveWidth(5),
+              borderRadius: responsiveWidth(8),
+              backgroundColor: "#000",
+              width: responsiveWidth(50),
+              marginTop: responsiveHeight(40),
+            }}
+          >
+            <Text
+              style={{
+                color: "#fff",
+                fontSize: responsiveFontSize(2),
+                fontWeight: "bold",
+              }}
+            >
+              Retry
+            </Text>
+          </TouchableOpacity>
+        )}
+      </SafeAreaView>
     </Fragment>
   );
-}
+};
 export default WallpaperHome;
